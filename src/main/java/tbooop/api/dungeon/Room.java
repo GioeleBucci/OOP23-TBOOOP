2fefced1378ed9565386bca7904920f836f93643
GioeleBucci <gioele.bucci@studio.unibo.it>
2023-11-26 13:53:28 +0100

    feat: add basic room
    added an abstract room and the relative interface

---
+++ src/main/java/tbooop/api/dungeon/Room.java
@@ -0,0 +1,36 @@
+package tbooop.api.dungeon;
+
+import java.util.Collection;
+import java.util.Set;
+
+import tbooop.commons.Directions;
+
+/**
+ * Base for all Rooms implementations.
+ * 
+ * @see {@link tbooop.model.dungeon.Floor Floor}
+ */
+public interface Room {
+    /**
+     * The doors that this room has.
+     * 
+     * @return a set of directions with an entry for every door present.
+     * @see Directions
+     */
+    Set<Directions> getDoorSet();
+
+    /**
+     * Takes a collection of directions and sets the doors of the room accordingly.
+     * 
+     * @param directions a collection of directions
+     * @see Directions
+     */
+    void setDoorSet(Collection<Directions> directions);
+
+    /**
+     * Whether this room has been visited at least once or not.
+     * 
+     * @return {@code true} if the room has been visited
+     */
+    boolean isExplored();
+}
---
+++ src/main/java/tbooop/model/dungeon/rooms/BaseRoom.java
@@ -0,0 +1,40 @@
+package tbooop.model.dungeon.rooms;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import tbooop.api.dungeon.Room;
+import tbooop.commons.Directions;
+
+/**
+ * Base for all Rooms implementations
+ * A room can have up to 4 doors and a series of rooms
+ * create a floor.
+ * 
+ * @see {@link tbooop.model.dungeon.Floor Floor}
+ */
+public abstract class BaseRoom implements Room {
+
+    private final Set<Directions> doorSet = new HashSet<>();
+    private boolean isExplored;
+
+    /** {@inheritDoc} */
+    @Override
+    public void setDoorSet(final Collection<Directions> directions) {
+        doorSet.addAll(directions);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<Directions> getDoorSet() {
+        return Collections.unmodifiableSet(doorSet);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean isExplored() {
+        return this.isExplored;
+    }
+}
