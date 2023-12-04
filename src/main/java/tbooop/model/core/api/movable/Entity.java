efe082bd385bde481e752b925d541e261c1648e0
Edoardo Ceresi <edoardo.ceresi@studio.unibo.it>
2023-12-04 15:06:39 +0100

    feat & refactor: add a javadoc for Entity.java and change the structure
    i've changed some method and added relative javadoc, this class is incomplete we need to finish some methods.

--- src/main/java/tbooop/model/core/api/movable/Entity.java
+++ src/main/java/tbooop/model/core/api/movable/Entity.java
@@ -5,44 +5,73 @@ import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 import javafx.geometry.Point2D;
 import tbooop.model.core.impl.GameObject;
 
-public abstract class Entity extends GameObject implements IDamageable {
-    
-    private int maxHealth;
-    private int currentHeath;
-    private Vector2D velocity;
+/**
+ * A Entity is an abstraction of anything that is Damageable and Movable in the game.
+ * Every object can take damage and can move must extends this abstact class.
+ */
 
-    protected Entity(Point2D position, int maxHealth, int currentHeath, Vector2D velocity) {
+public abstract class Entity extends GameObject implements IDamageable {
+
+    private int maxHealth;      // NOPMD suppressed as it is a false positive
+    private int currentHealth;
+    private Vector2D velocity;  // NOPMD suppressed as it is a false positive
+
+    /**
+     * Create a new istance of a Entity.
+     * 
+     * @param position starting position (as a {@link javafx.geometry.Point2D
+     *                 Point2D})
+     * @param maxHealth it is the maximum starting health
+     * @param currentHealth it is the current health
+     * @param velocity it is the Entity velocity 
+     * @throws NullPointerException if any parameter passed is null
+     */
+
+    protected Entity(final Point2D position, final int maxHealth, final int currentHealth, final Vector2D velocity) {
         super(position);
-        this.currentHeath = currentHeath;
+        this.currentHealth = currentHealth;
         this.maxHealth = maxHealth;
         this.velocity = velocity;
     }
 
-    public void update(long deltaTime) {
-        //TO DO to implements
+    /** {@inheritDoc} */
+    @Override
+    public void update(final long deltaTime) {
+        //TO DO to implements.
     }
 
+    /** {@inheritDoc} */
+    @Override
     public abstract void onCollision(GameObject gameObj);
 
-    public void takeDamage(int damage) {
-        this.currentHeath = this.currentHeath - damage;        
+    /** {@inheritDoc} */
+    @Override
+    public void takeDamage(final int damage) {
+        this.currentHealth = this.currentHealth - damage;
     }
 
+    /** {@inheritDoc} */
+    @Override
     public abstract void die();
 
+    /** {@inheritDoc} */
+    @Override
     public int getHealth() {
-        return this.currentHeath;
+        return this.currentHealth;
     }
 
+    /** {@inheritDoc} */
+    @Override
     public int getMaxHealth() {
-        return this.maxHealth;    
+        return this.maxHealth;
     }
 
-    public void setVelocity(Vector2D velocity) {
-        this.velocity = velocity;
-    }
-
-    public Vector2D getVelocity() {
-        return this.velocity;
+    /**
+     * This method it is used to get the velocity with a safe method.
+     * @return immutableVelocity
+     */
+    protected Vector2D getVelocity() {
+        final Vector2D immutableVelocity = this.velocity;
+        return immutableVelocity; // NOPMD suppressed as it is a false positive
     }
 }
