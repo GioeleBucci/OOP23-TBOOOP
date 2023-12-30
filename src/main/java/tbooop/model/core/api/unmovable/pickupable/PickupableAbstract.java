cabcc69b5789ea0b3570c15febff8cd1bbe030db
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2023-12-30 16:28:42 +0100

    feat: add PickupableAbstract class
    added Pickupable abstract class which implements
    methods shared by all the items and declares methods
    to be implemented by single different items

---
+++ src/main/java/tbooop/model/core/api/unmovable/pickupable/PickupableAbstract.java
@@ -0,0 +1,23 @@
+package tbooop.model.core.api.unmovable.pickupable;
+
+/**
+ * Abstract class for pickupable items
+ * <p>
+ * Implements methods from Pickupable
+ * interface (equals for every item)
+ * and declare abstract methods to
+ * be implemented by single different
+ * classes.
+ */
+public abstract class PickupableAbstract implements Pickupable{
+    
+    public void onCollision(){
+    }
+
+    /**
+    * Determines the effect of the
+    * item picked up by the 
+    * player.
+    */
+    public abstract void onPickup();
+}
