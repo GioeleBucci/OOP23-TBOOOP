9350ca95fcc99f552f33450c6fc8d7d872baa399
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2024-01-05 18:41:37 +0100

    feat: add new Heart class
    it rapresents Heart item in the game (pickupable item
    by the player)

---
+++ src/main/java/tbooop/model/core/api/unmovable/pickupable/Heart.java
@@ -0,0 +1,25 @@
+package tbooop.model.core.api.unmovable.pickupable;
+
+import tbooop.commons.Point2d;
+
+public class Heart extends PickupableAbstract{
+
+    @Override
+    public Point2d getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public void setPosition(Point2d newPos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPosition'");
+    }
+
+    @Override
+    public void onPickup() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onPickup'");
+    }
+
+}
