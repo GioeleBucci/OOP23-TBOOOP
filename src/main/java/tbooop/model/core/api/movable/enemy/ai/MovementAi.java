d83ebdd229c7198feef501156a5e44e93ff78d42
Simone Mosconi <simone.mosconi2@studio.unibo.it>
2024-01-08 19:50:13 +0100

    feat: add new package "ai", add "MovementAi" interface.
    In the enemy package there's now a new package for ai classes, wich are used by enemies.

---
+++ src/main/java/tbooop/model/core/api/movable/enemy/ai/MovementAi.java
@@ -0,0 +1,9 @@
+package tbooop.model.core.api.movable.enemy.ai;
+
+/**
+ * A movement ai is a class that contains an algorithm which tells an
+ * enemy class where it should move on each update.
+ */
+public interface MovementAi {
+
+}
