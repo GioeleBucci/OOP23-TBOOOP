22369ddb23516d0292f7a7450abe4b87d0ca93c5
Gioele Bucci <gioele.bucci@studio.unibo.it>
2024-01-11 09:00:36 +0100

    feat add an utility class for room bounds detection
    added a static RoomBounds class, which can be used in movement calculations to determine if a calcualted
    position is legal (inside the room) or not

---
+++ src/main/java/tbooop/commons/RoomBounds.java
@@ -0,0 +1,36 @@
+package tbooop.commons;
+
+/**
+ * Dimensions for the rooms.
+ * <br>
+ * These informations can be used for movement calculations.
+ */
+public final class RoomBounds {
+
+    /**
+     * The height of a room (not necessairly in pixels).
+     */
+    public static final double HEIGHT = 364;
+    /**
+     * The width of a room (not necessairly in pixels).
+     */
+    public static final double WIDTH = 207;
+
+    /**
+     * Whether the position passed is out of room bounds.
+     * 
+     * @param position the position to check
+     * @return {@code true} if the position is out of bounds
+     */
+    public static boolean outOfBounds(final Point2d position) {
+        return position.getX() < 0 || position.getX() >= WIDTH
+                || position.getY() < 0 || position.getY() >= HEIGHT;
+    }
+
+    /**
+     * Don't let anyone instantiate this class.
+     */
+    private RoomBounds() {
+    }
+
+}
