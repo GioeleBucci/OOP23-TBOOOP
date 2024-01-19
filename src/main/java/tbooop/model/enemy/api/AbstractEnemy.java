228195408ff3d93b4c68220488ff4611743f647d
Simone Mosconi <simone.mosconi2@studio.unibo.it>
2024-01-19 01:12:19 +0100

    feat: add fields "player" and "ai" in AbstractEnemy, implement abstract methods "onCollision" and "updateState" in AbstractEnemy, add "move" method in AbstractEnemy, add "newPosition" method in MovementAi.

--- src/main/java/tbooop/model/enemy/api/AbstractEnemy.java
+++ src/main/java/tbooop/model/enemy/api/AbstractEnemy.java
@@ -1,14 +1,23 @@
 package tbooop.model.enemy.api;
 
 import tbooop.commons.Point2d;
+import tbooop.commons.RoomBounds;
 import tbooop.commons.api.Health;
+import tbooop.model.core.api.GameObject;
 import tbooop.model.core.api.GameTag;
 import tbooop.model.core.api.movable.Entity;
+import tbooop.model.enemy.api.ai.MovementAi;
+import tbooop.model.player.api.Player;
+
+import java.util.Objects;
 
 /**
  * Abstract Enemy class which contains the essential components of an enemy.
  */
 public abstract class AbstractEnemy extends Entity implements Enemy {
+
+    private final Player player;
+    private final MovementAi ai;
 
     /**
      * creates a new istance of an Enemy.
@@ -16,12 +25,46 @@ public abstract class AbstractEnemy extends Entity implements Enemy {
      * @param position on the 2D map
      * @param health the enemy's health
      * @param velocity determines how fast the enemy moves
+     * @param player the player of the game
+     * @param ai the enemy's movement ai
      */
     protected AbstractEnemy(
         final Point2d position,
         final Health health,
-        final double velocity) {
+        final double velocity,
+        final Player player,
+        final MovementAi ai) {
         super(position, health, velocity, GameTag.ENEMY);
-        //TODO Auto-generated constructor stub
+        this.player = Objects.requireNonNull(player);
+        this.ai = Objects.requireNonNull(ai);
     }
+
+    // DA DISCUTERE!
+    /** {@inheritDoc} */
+    @Override
+    public void onCollision(final GameObject gameObj) {
+        if (gameObj.getTag().equals(GameTag.PLAYER)) {
+            this.player.takeDamage(1);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void updateState(final long deltaTime) {
+        this.move(deltaTime);
+    }
+
+    /**
+     * Updates the enemy's position.
+     * 
+     * @param deltaTime the time passed since the previous position update
+     */
+    protected void move(final long deltaTime) {
+        final Point2d newPos = this.ai.newPosition(
+            super.getPosition(), deltaTime);
+        if (!RoomBounds.outOfBounds(newPos)) {
+            super.setPosition(newPos);
+        }
+    }
+
 }
