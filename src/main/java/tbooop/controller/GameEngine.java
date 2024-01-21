9e8b29ee021bef7bc06f7957043b332517aa88ef
Edoardo Ceresi <edoardo.ceresi@studio.unibo.it>
2024-01-21 18:42:41 +0100

    feat: add a field cmdQueue and a method notifyCommand in GameEngine.java

--- src/main/java/tbooop/controller/GameEngine.java
+++ src/main/java/tbooop/controller/GameEngine.java
@@ -2,20 +2,27 @@ package tbooop.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
+import tbooop.controller.api.CommandListener;
 import tbooop.controller.api.Event;
 import tbooop.controller.api.EventListener;
+import tbooop.controller.api.PlayerCommand;
+
 import java.util.logging.Logger;
 
 /**
  * The GameEngine class is responsible for managing the game loop and processing
  * events.
  */
-public final class GameEngine implements EventListener {
+public final class GameEngine implements EventListener, CommandListener {
 
     private final List<Event> eventQueue = new ArrayList<>();
     // private final World world = new World();
     private final Logger logger = Logger.getLogger(GameEngine.class.getName());
+    private final BlockingQueue<PlayerCommand> cmdQueue = new ArrayBlockingQueue<>(100);
 
     private static final int FPS = 60;
     private static final long REFRESH_PERIOD = 1L / FPS * 1000; // in ms
@@ -47,6 +54,12 @@ public final class GameEngine implements EventListener {
     public void notifyEvent(final Event event) {
         logger.info("new event recieved.");
         eventQueue.add(event);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void notifyCommand(final PlayerCommand cmd) {
+        this.cmdQueue.add(Objects.requireNonNull(cmd));
     }
 
     private void waitForNextFrame(final long startTime) {
