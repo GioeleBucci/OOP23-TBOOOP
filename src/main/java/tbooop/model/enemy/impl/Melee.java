3f3652f4fe16949a81a27c2eee9669a6362aaf0d
Simone Mosconi <simone.mosconi2@studio.unibo.it>
2024-01-22 00:09:08 +0100

    feat: add "Melee" decoration class.

---
+++ src/main/java/tbooop/model/enemy/impl/Melee.java
@@ -0,0 +1,21 @@
+package tbooop.model.enemy.impl;
+
+import tbooop.model.enemy.api.Enemy;
+import tbooop.model.enemy.api.EnemyDecorator;
+
+/**
+ * a Melee decorator makes the enemy deal more damage when
+ * a physical collision with the player happens.
+ */
+public class Melee extends EnemyDecorator {
+
+    /**
+     * Creates an instance of a Melee decoration.
+     * 
+     * @param concreteEnemy the enemy that gets decorated.
+     */
+    protected Melee(final Enemy concreteEnemy) {
+        super(concreteEnemy);
+    }
+
+}
