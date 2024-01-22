e9e07e81f9efcc4fefc6e3073792f2f5d3d6a3c0
Edoardo Ceresi <edoardo.ceresi@studio.unibo.it>
2024-01-22 17:14:00 +0100

    feat: add a new class ShootCommand.java

---
+++ src/main/java/tbooop/controller/ShootCommand.java
@@ -0,0 +1,15 @@
+package tbooop.controller;
+
+import tbooop.controller.api.PlayerCommand;
+import tbooop.model.player.api.Player;
+
+public class ShootCommand implements PlayerCommand{
+
+    /** {@ineritDoc} */
+    @Override
+    public void execute(Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
