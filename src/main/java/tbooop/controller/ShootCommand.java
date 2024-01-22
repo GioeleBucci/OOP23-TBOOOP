06ba7418993dcafb912583b0f2724fbb51d5dc2b
Edoardo Ceresi <edoardo.ceresi@studio.unibo.it>
2024-01-22 17:28:04 +0100

    feat: add a new field a new constructor in ShootCommand.java

--- src/main/java/tbooop/controller/ShootCommand.java
+++ src/main/java/tbooop/controller/ShootCommand.java
@@ -1,15 +1,30 @@
 package tbooop.controller;
 
+import tbooop.commons.Point2ds;
 import tbooop.controller.api.PlayerCommand;
 import tbooop.model.player.api.Player;
 
-public class ShootCommand implements PlayerCommand{
+/**
+ * A command to shoot in a specific direction.
+ */
+public class ShootCommand implements PlayerCommand {
 
-    /** {@ineritDoc} */
+    private final Point2ds direction;
+
+    /**
+     * Constructs a ShootCommand object with the specified direction.
+     * 
+     * @param direction the direction in which the player should shoot
+     */
+    public ShootCommand(final Point2ds direction) {
+        this.direction = direction;
+    }
+
+    /** {@inheritDoc} */
     @Override
-    public void execute(Player player) {
+    public void execute(final Player player) {
+        player.move(direction);
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'execute'");
     }
-    
 }
