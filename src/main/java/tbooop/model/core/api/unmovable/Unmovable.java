e219e49e82f239ac101b3b83835595892f98d60f
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2024-01-24 17:04:41 +0100

    feat: add onPickup method declaration in Unmovable interface

--- src/main/java/tbooop/model/core/api/unmovable/Unmovable.java
+++ src/main/java/tbooop/model/core/api/unmovable/Unmovable.java
@@ -1,6 +1,7 @@
 package tbooop.model.core.api.unmovable;
 
 import tbooop.model.core.api.GameObject;
+import tbooop.model.player.api.Player;
 
 /**
  * Interface for unmovable game object.
@@ -10,4 +11,12 @@ import tbooop.model.core.api.GameObject;
 
 public interface Unmovable extends GameObject {
 
+    /**
+     * Determines the effect of the
+     * item picked up by the
+     * player.
+     * 
+     * @param player istance of player
+     */
+    void onPickup(Player player);
 }
