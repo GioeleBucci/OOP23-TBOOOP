b664c0ce0b4e2c629b6a089930b09e0b39d02072
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2024-01-25 23:28:04 +0100

    feat: create new ItemShopLogic class

---
+++ src/main/java/tbooop/model/pickupables/items/impl/ItemShopLogic.java
@@ -0,0 +1,12 @@
+package tbooop.model.pickupables.items.impl;
+
+/**
+ * Class implementing item's handling logic
+ * in the item shop room.
+ */
+public class ItemShopLogic {
+
+    //private Map<Items, Items> itemsMap = new HashMap<>();
+    //private Point2d location = new Point2dImpl(1.0, 1.0);
+    //private double itemColliderRadius = 1.0;
+}
