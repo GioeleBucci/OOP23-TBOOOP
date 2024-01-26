7d5e8e429e6b782a17f54a6f422329006d054134
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2024-01-26 18:15:01 +0100

    feat: add getItemsPool method
    this method returns a map from an Item to its relative shop-price label

--- src/main/java/tbooop/model/pickupables/items/impl/ItemShopLogic.java
+++ src/main/java/tbooop/model/pickupables/items/impl/ItemShopLogic.java
@@ -1,12 +1,34 @@
 package tbooop.model.pickupables.items.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import tbooop.commons.Point2dImpl;
+import tbooop.commons.api.Point2d;
+import tbooop.model.core.api.GameTag;
+import tbooop.model.pickupables.items.api.Items;
+
 /**
  * Class implementing item's handling logic
  * in the item shop room.
  */
 public class ItemShopLogic {
 
-    //private Map<Items, Items> itemsMap = new HashMap<>();
-    //private Point2d location = new Point2dImpl(1.0, 1.0);
-    //private double itemColliderRadius = 1.0;
+    private final Point2d location = new Point2dImpl(1.0, 1.0);
+    private static final double ITEM_COLLIDER_RADIUS = 1.0;
+
+    /**
+     * Returns a map from an item to its
+     * price label.
+     * It will be used to shown the item and
+     * relative price in the item shop.
+     * 
+     * @return a map from Items to Items
+     */
+    public Map<Items, Items> getItemsPool() {
+        final Map<Items, Items> itemsMap = new HashMap<>();
+        itemsMap.put(new GoldenHeart(location, ITEM_COLLIDER_RADIUS, GameTag.PICKUP), 
+                    new ShopCoinLabel(location, ITEM_COLLIDER_RADIUS, GameTag.PICKUP));
+        return itemsMap;
+    }
 }
