3b603c6a274c45873b289d44f4a96526649e238d
Edoardo Ceresi <edoardo.ceresi@studio.unibo.it>
2024-01-26 13:53:31 +0100

    feat: add a new method setDamage in PlayerProjectile.java

--- src/main/java/tbooop/model/player/api/PlayerProjectile.java
+++ src/main/java/tbooop/model/player/api/PlayerProjectile.java
@@ -1,5 +1,17 @@
 package tbooop.model.player.api;
 
-public interface PlayerProjectile {
+import tbooop.commons.api.Projectile;
+
+/**
+ * A Player projectile is movable GameObject that travels in a straight line.
+ * It's not damageable, however it can damage Enemies.
+ * It disappears when colliding either into a map border or an Enemies.
+ */
+public interface PlayerProjectile extends Projectile{
     
+    /**
+    * This method set the amount of damage the Player can do.
+    * @param amount the amount of damage to set.
+    */
+    void setDamage(int amount);
 }
