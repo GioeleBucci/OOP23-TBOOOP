4178dfc9f3b014967a072a9bc68ba920461c94fb
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2024-01-28 18:22:04 +0100

    feat: add new Items to ItemRoomLogic class, with relative implementation

--- src/main/java/tbooop/model/pickupables/items/impl/ItemRoomLogic.java
+++ src/main/java/tbooop/model/pickupables/items/impl/ItemRoomLogic.java
@@ -16,8 +16,13 @@ public class ItemRoomLogic {
     private final Point2d location = new Point2dImpl(1.0, 1.0);
     private static final double ITEM_COLLIDER_RADIUS = 1.0;
     private static final double GOLDENHEART_PROB = 0.1;
-    private static final double ANOTHERITEM_PROB = 0.3;
-    private final List<Items> itemsList = List.of(new GoldenHeart(location, ITEM_COLLIDER_RADIUS, GameTag.PICKUP));
+    private static final double BELT_PROB = 0.2;
+    private static final double IRONBAR_PROB = 0.3;
+    private static final double FIREMIND_PROB = 0.4;
+    private final List<Items> itemsList = List.of(new GoldenHeart(location, ITEM_COLLIDER_RADIUS, GameTag.PICKUP),
+                                                new Belt(location, ITEM_COLLIDER_RADIUS, GameTag.PICKUP),
+                                                new IronBar(location, ITEM_COLLIDER_RADIUS, GameTag.PICKUP),
+                                                new FireMind(location, ITEM_COLLIDER_RADIUS, GameTag.PICKUP));
 
     /**
      * Compute the chances every item has
@@ -30,10 +35,12 @@ public class ItemRoomLogic {
     private int getChanceToSpawn(final double randomNumber) {
         if (randomNumber <= GOLDENHEART_PROB) {
             return 0;
-        } else if (randomNumber > GOLDENHEART_PROB && randomNumber <= ANOTHERITEM_PROB) {
+        } else if (randomNumber > GOLDENHEART_PROB && randomNumber <= (GOLDENHEART_PROB + BELT_PROB)) {
             return 1;
-        } else {
+        } else if (randomNumber > IRONBAR_PROB && randomNumber <= (IRONBAR_PROB + FIREMIND_PROB)) {
             return 2;
+        } else {
+            return 3;
         }
     }
 
