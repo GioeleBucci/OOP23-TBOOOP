32f45762629eb447d9e17d0d8323b37f79133594
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2024-01-28 18:05:29 +0100

    feat: add new items generation in the ItemShopLogic class

--- src/main/java/tbooop/model/pickupables/items/impl/ItemShopLogic.java
+++ src/main/java/tbooop/model/pickupables/items/impl/ItemShopLogic.java
@@ -17,10 +17,13 @@ public class ItemShopLogic {
     private final Point2d location = new Point2dImpl(1.0, 1.0);
     private static final double ITEM_COLLIDER_RADIUS = 1.0;
     private final Items goldenHeart = new GoldenHeart(location, ITEM_COLLIDER_RADIUS, GameTag.PICKUP);
+    private final Items belt = new Belt(location, ITEM_COLLIDER_RADIUS, GameTag.PICKUP);
+    private final Items fireMind = new FireMind(location, ITEM_COLLIDER_RADIUS, GameTag.PICKUP);
+    private final Items ironBar = new IronBar(location, ITEM_COLLIDER_RADIUS, GameTag.PICKUP);
 
     /**
      * Returns a map from an item to its
-     * price label.
+     * price.
      * It will be used to shown the item and
      * relative price in the item shop.
      * 
@@ -28,7 +31,14 @@ public class ItemShopLogic {
      */
     public Map<Items, Integer> getItemsPool() {
         final Map<Items, Integer> itemsMap = new HashMap<>();
+        goldenHeart.setInShop();
         itemsMap.put(goldenHeart, goldenHeart.getPrice());
+        belt.setInShop();
+        itemsMap.put(belt, belt.getPrice());
+        fireMind.setInShop();
+        itemsMap.put(fireMind, fireMind.getPrice());
+        ironBar.setInShop();
+        itemsMap.put(ironBar, ironBar.getPrice());
 
         return itemsMap;
     }
