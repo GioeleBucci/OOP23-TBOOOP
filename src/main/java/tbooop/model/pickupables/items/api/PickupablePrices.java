5ad09b6bd36963832cabd258d7f71d7ebe817e8e
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2024-01-28 17:59:58 +0100

    feat: add prices for new items in the enum price class

--- src/main/java/tbooop/model/pickupables/items/api/PickupablePrices.java
+++ src/main/java/tbooop/model/pickupables/items/api/PickupablePrices.java
@@ -6,7 +6,13 @@ package tbooop.model.pickupables.items.api;
  */
 public enum PickupablePrices {
     /**Price for GoldenHeart item.*/
-    GOLDENHEART_PRICE(15);
+    GOLDENHEART_PRICE(15),
+    /**Price for the Belt item. */
+    BELT_PRICE(10),
+    /**Price for FireMind item. */
+    FIREMIND_PRICE(20),
+    /**Price for Iron Bar item. */
+    IRONBAR_PRICE(15);
 
     private int itemPrice;
     /**Getter for the value of every item price.
