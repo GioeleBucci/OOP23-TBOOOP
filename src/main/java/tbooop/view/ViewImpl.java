4fe55c15eee1dc7da99d45a18e5afb29799bb2af
Gioele Bucci <gioele.bucci@studio.unibo.it>
2024-01-31 21:32:11 +0100

    fix: fix scaling issue with gui

--- src/main/java/tbooop/view/ViewImpl.java
+++ src/main/java/tbooop/view/ViewImpl.java
@@ -4,25 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-import tbooop.commons.HealthImpl;
 import tbooop.commons.Point2dImpl;
 import tbooop.commons.RoomBounds;
 import tbooop.commons.api.Point2d;
 import tbooop.controller.ControllerImpl;
 import tbooop.controller.api.Controller;
 import tbooop.model.core.api.GameObject;
-import tbooop.model.dungeon.rooms.api.Room;
-import tbooop.model.player.impl.PlayerImpl;
 import tbooop.view.api.View;
 
 /**
@@ -33,14 +27,9 @@ public final class ViewImpl extends Application implements View {
     private final Map<GameObject, ImageView> map = new HashMap<>();
 
     private final Logger logger = Logger.getLogger(ViewImpl.class.getName());
-    // private static final double INITIAL_HEIGHT =
-    // Screen.getPrimary().getVisualBounds().getHeight();
     public static final double BASE_ROOM_W = 468;
     public static final double BASE_ROOM_H = 311;
-    private static final double WALL_THICKNESS = BASE_ROOM_W - RoomBounds.WIDTH / 2;
 
-    // private static final double INITIAL_WIDTH = BASE_ROOM_H * RoomBounds.WIDTH /
-    // RoomBounds.HEIGHT;
     private final Group root = new Group();
     private final Controller controller = new ControllerImpl(this);
     private Scene scene;
@@ -56,6 +45,9 @@ public final class ViewImpl extends Application implements View {
 
     @Override
     public void start(final Stage primaryStage) {
+
+        
+
         this.stage = primaryStage;
 
         primaryStage.setResizable(false);
@@ -75,9 +67,7 @@ public final class ViewImpl extends Application implements View {
 
         // TEST
         double upscaleFactor = scene.getWidth() / BASE_ROOM_W;
-        rect = new Rectangle(RoomBounds.WIDTH * upscaleFactor, RoomBounds.HEIGHT * upscaleFactor, Color.RED);
-        // rect.setX(scene.getWidth() / 2 - rect.getWidth() / 2);
-        // rect.setY(scene.getHeight() / 2 - rect.getHeight() / 2);
+        rect = new Rectangle(RoomBounds.WIDTH * upscaleFactor, RoomBounds.HEIGHT * upscaleFactor, Color.TRANSPARENT);
 
         // Bind the image view's size to the scene's size
         rect.widthProperty()
@@ -98,7 +88,6 @@ public final class ViewImpl extends Application implements View {
         /// TEST CALCULATE WINDOW BAR SIZE
         barsize = primaryStage.getHeight() - scene.getHeight();
         System.out.println("barsize is " + barsize);
-
         /// END TEST
 
         final Thread thread = new Thread(() -> {
@@ -116,10 +105,18 @@ public final class ViewImpl extends Application implements View {
      */
     private void updateView() {
         for (final var entry : map.entrySet()) {
-            final Point2d newPos = worldToScreenPos(entry.getKey().getPosition());
+            Point2d newPos = worldToScreenPos(entry.getKey().getPosition());
+            // subtract half the image size to center the image
+            newPos = newPos.subtract(new Point2dImpl(
+                    entry.getValue().getFitWidth() / 2,
+                    entry.getValue().getFitHeight() / 2));
             final ImageView imgView = entry.getValue();
-            imgView.setTranslateX(newPos.getX() - imgView.getX() - imgView.getImage().getWidth() / 2);
-            imgView.setTranslateY(newPos.getY() - imgView.getY() - imgView.getImage().getHeight() / 2);
+            logger.info("Model pos:" + entry.getKey().getPosition().toString()
+                    + "\nScreen pos: " + newPos.toString()
+                    + "\nImage size: " + entry.getValue().getFitWidth() + "x" + entry.getValue().getFitHeight()
+                    + "\nScreen size: " + scene.getWidth() + "x" + scene.getHeight());
+            imgView.setX(newPos.getX()); // - imgView.getX() - imgView.getImage().getWidth() / 2);
+            imgView.setY(newPos.getY()); // - imgView.getY() - imgView.getImage().getHeight() / 2);
         }
         rect.setTranslateX(scene.getWidth() / 2 - rect.getWidth() / 2);
         rect.setTranslateY(scene.getHeight() / 2 - rect.getHeight() / 2);
@@ -152,18 +149,6 @@ public final class ViewImpl extends Application implements View {
         final ImageView imgView = new ImageView(img);
 
         map.put(gobj, imgView);
-
-        // // Set the translation
-        // imgView.setX(position.getX());
-        // imgView.setY(position.getY());
-
-        final Point2d startPos = worldToScreenPos(gobj.getPosition());
-
-        // Bind the image view's position to the scene's size
-        imgView.xProperty().bind(rect.widthProperty()
-                .multiply(startPos.getX() / rect.widthProperty().get()));
-        imgView.yProperty().bind(rect.heightProperty()
-                .multiply(startPos.getY() / rect.heightProperty().get()));
 
         // Bind the image view's size to the scene's size
         imgView.fitWidthProperty()
