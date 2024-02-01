650ab8c87a54e543cf67d763d73eec06da167f57
Edoardo Ceresi <edoardo.ceresi@studio.unibo.it>
2024-02-01 13:38:54 +0100

    doc: add javadoc in HeathRender

--- src/main/java/tbooop/view/HealthRender.java
+++ src/main/java/tbooop/view/HealthRender.java
@@ -6,13 +6,18 @@ import javafx.scene.Group;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/** Renders a Hearth. */
 public class HealthRender {
-    
+
     private int scale;
     private static final int SCALE_PERCENTAGE = 25;
     private final Group root = new Group();
     private List<ImageView> heartList = new ArrayList<>();
 
+    /** 
+     * @param parentRoot the root this attaches to
+     *  @param initialHealth the initial Player health
+     */
     public HealthRender(final Group parentRoot, final int initialHealth) {
         parentRoot.getChildren().add(this.root);
         init(initialHealth);
@@ -30,14 +35,25 @@ public class HealthRender {
         }
     }
 
+    /**
+     * Change the empty heart with the full one.
+     * @param currentHealth the heart to change
+     */
     public void toggledHealth(final int currentHealth) {
         this.heartList.get(currentHealth).setImage(new Image("empty_hearth.png"));
     }
 
+    /**
+     * Change the full heart with the empty one.
+     * @param currentHealth the heart to change
+     */
     public void addHealth(final int currentHealth) {
         this.heartList.get(currentHealth - 1).setImage(new Image("full_hearth.png")); 
     }
 
+    /**
+     * Add a new Heart.
+     */
     public void addMaxHealth() {
         ImageView heartView = new ImageView("full_hearth.png");
         root.getChildren().add(heartView);
