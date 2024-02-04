e2404ece02182613f8f1353e040ff465632a6a17
Simone Mosconi <simone.mosconi2@studio.unibo.it>
2024-02-04 13:50:46 +0100

    feat: add EnemyType enum.

---
+++ src/main/java/tbooop/model/enemy/api/EnemyType.java
@@ -0,0 +1,11 @@
+package tbooop.model.enemy.api;
+
+/**
+ * enum to classify different types of enemies. 
+ */
+public enum EnemyType {
+    /** an enemy that deals damage on physical contact. */
+    MELEE,
+    /** an enemy that shoots projectiles. */
+    SHOOTER;
+}
