27b433e54394e5bd2cee7ab01614555f9c1ccf99
Gioele Bucci <gioele.bucci@studio.unibo.it>
2024-02-05 17:50:34 +0100

    feat: add room renderer
    This class is responsible for drawing doors in the view

--- src/main/java/tbooop/view/RoomRenderer.java
+++ src/main/java/tbooop/view/RoomRenderer.java
@@ -1,16 +1,89 @@
 package tbooop.view;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.application.Platform;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import tbooop.commons.Point2ds;
+import tbooop.model.dungeon.rooms.api.DoorUnmodifiable;
+import tbooop.model.dungeon.rooms.api.RoomUnmodifiable;
 import tbooop.view.api.ViewComponent;
 import tbooop.view.api.ViewElements;
 
+/**
+ * The RoomRenderer class is responsible for rendering the room and its doors in
+ * the game view.
+ */
 public class RoomRenderer extends ViewComponent {
+    private static final Image REGULAR_DOOR_OPEN = new Image("tileset/door_open.png");
+    private static final Image REGULAR_DOOR_CLOSED = new Image("tileset/door_closed.png");
+    private static final Image SPECIAL_DOOR_OPEN = new Image("tileset/special_door_open.png");
+    private static final Image SPECIAL_DOOR_CLOSED = new Image("tileset/special_door_closed.png");
 
+    private final Map<DoorUnmodifiable, ImageView> doorsSpriteMap = new HashMap<>();
+
+    /**
+     * Constructs a RoomRenderer object with the specified view.
+     * 
+     * @param view the view that will be updated
+     */
     public RoomRenderer(final ViewElements view) {
         super(view);
     }
 
+    /** {@inheritDoc} */
+    @Override
+    public void init() {
+    }
+
+    /** {@inheritDoc} */
     @Override
     public void update() {
+    }
+
+    /**
+     * Change the room to render.
+     * 
+     * @param newRoom the new room to render
+     */
+    public void changeRoom(final RoomUnmodifiable newRoom) {
+        Platform.runLater(() -> {
+            // clear doors
+            getView().getRoot().getChildren().removeAll(doorsSpriteMap.values());
+            newRoom.getDoorMap().entrySet().forEach(e -> {
+                ImageView imgView;
+                if (e.getValue().isSpecial()) {
+                    imgView = new ImageView(
+                            e.getValue().isOpen() ? SPECIAL_DOOR_OPEN : SPECIAL_DOOR_CLOSED);
+                } else {
+                    imgView = new ImageView(
+                            e.getValue().isOpen() ? REGULAR_DOOR_OPEN : REGULAR_DOOR_CLOSED);
+                }
+                ((ViewImpl) getView()).addGameObjectToView(imgView, e.getValue());
+                doorsSpriteMap.put(e.getValue(), imgView);
+                rotateDoor(imgView, e.getKey());
+            });
+        });
+    }
+
+    private void rotateDoor(final ImageView imgView, final Point2ds dir) {
+        switch (dir) {
+            case DOWN -> imgView.setRotate(180);
+            case LEFT -> imgView.setRotate(90 * -1);
+            case RIGHT -> imgView.setRotate(90);
+            default -> {
+            }
+        }
+    }
+
+    private void setDoorImage(final ImageView doorView, final DoorUnmodifiable door) {
+        if (door.isSpecial()) {
+            doorView.setImage(door.isOpen() ? SPECIAL_DOOR_OPEN : SPECIAL_DOOR_CLOSED);
+        } else {
+            doorView.setImage(door.isOpen() ? REGULAR_DOOR_OPEN : REGULAR_DOOR_CLOSED);
+        }
     }
 
 }
