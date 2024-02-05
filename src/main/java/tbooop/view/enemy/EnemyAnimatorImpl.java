69469cda621a82dba16155626d672f6b8e893858
Simone Mosconi <simone.mosconi2@studio.unibo.it>
2024-02-05 23:29:14 +0100

    refactor: extract interface "EnemyAnimator" from EnemyAnimatorImpl.

--- src/main/java/tbooop/view/enemy/EnemyAnimatorImpl.java
+++ src/main/java/tbooop/view/enemy/EnemyAnimatorImpl.java
@@ -9,22 +9,23 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import tbooop.model.core.api.GameObjectUnmodifiable;
 import tbooop.model.enemy.api.Enemy;
+import tbooop.view.api.enemy.EnemyAnimator;
 
 /**
- * An enemy animator stores the logic that allows the view
- * to display a graphical animation of enemies.
+ * Implementation of EnemyAnimator, this class directly access the view's gameObjmap to animate
+ * the enemies contained in it.
+ * @see EnemyAnimator
  */
 @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "Due to design decisions"
         + "this class should be able to access to the view's map of gameObjects in order to"
         + "easily update the animation frames of only the enemies that may be present.")
-public class EnemyAnimatorImpl {
+public class EnemyAnimatorImpl implements EnemyAnimator {
 
     private static final int MELEE_UPDATE_FREQUENCY = 1000 / 12;
     private static final int SHOOTER_UPDATE_FREQUENCY = 1000 / 8;
     private static final int BOUNCER_UPDATE_FREQUENCY = 1000 / 4;
 
-
-    private final EnemyFrameUpdaterImpl meleeUpdater = new EnemyFrameUpdaterImpl(List.of(
+    private final EnemyFrameUpdater meleeUpdater = new EnemyFrameUpdater(List.of(
         new Image("enemy/melee/melee1.png"),
         new Image("enemy/melee/melee2.png"),
         new Image("enemy/melee/melee3.png"),
@@ -36,13 +37,13 @@ public class EnemyAnimatorImpl {
         new Image("enemy/melee/melee9.png"),
         new Image("enemy/melee/melee10.png"),
         new Image("enemy/melee/melee11.png")), MELEE_UPDATE_FREQUENCY);
-    private final EnemyFrameUpdaterImpl shooterUpdater = new EnemyFrameUpdaterImpl(List.of(
+    private final EnemyFrameUpdater shooterUpdater = new EnemyFrameUpdater(List.of(
         new Image("enemy/shooter/shooter1.png"),
         new Image("enemy/shooter/shooter2.png"),
         new Image("enemy/shooter/shooter3.png"),
         new Image("enemy/shooter/shooter4.png"),
         new Image("enemy/shooter/shooter5.png")), SHOOTER_UPDATE_FREQUENCY);
-    private final EnemyFrameUpdaterImpl bouncerUpdater = new EnemyFrameUpdaterImpl(List.of(
+    private final EnemyFrameUpdater bouncerUpdater = new EnemyFrameUpdater(List.of(
         new Image("enemy/bouncer/bouncer1.png"),
         new Image("enemy/bouncer/bouncer2.png"),
         new Image("enemy/bouncer/bouncer3.png")), BOUNCER_UPDATE_FREQUENCY);
@@ -58,9 +59,8 @@ public class EnemyAnimatorImpl {
         this.gameObjMap = Objects.requireNonNull(gameObjMap);
     }
 
-    /**
-     * This method updates the animation frames of enemies stored in gameObjMap.
-     */
+    /** {@inheritDoc} */
+    @Override
     public void update() {
         final long currentTime = System.currentTimeMillis();
         for (final var en : gameObjMap.entrySet()) {
