cd4eb05d6b4c7498d949815780365f9c68cb75ea
Gioele Bucci <gioele.bucci@studio.unibo.it>
2024-02-06 10:37:38 +0100

    refactor: extract world methods to interface

---
+++ src/main/java/tbooop/controller/api/World.java
@@ -0,0 +1,46 @@
+package tbooop.controller.api;
+
+import java.util.Set;
+import tbooop.commons.api.Projectile;
+import tbooop.model.core.api.GameObject;
+import tbooop.model.dungeon.rooms.api.DoorUnmodifiable;
+import tbooop.model.player.api.Player;
+
+/**
+ * The World class represents the game world and contains information about the
+ * game objects, the player and the current floor.
+ */
+public interface World extends ControllerComponent {
+
+    /**
+     * Returns the list of game objects in the world.
+     *
+     * @return the list of game objects
+     */
+    Set<GameObject> getGameObjects();
+
+    /**
+     * Returns the list of game objects in the world.
+     *
+     * @return the list of game objects
+     */
+    Set<Projectile> getProjectiles();
+
+    /**
+     * Returns the player in the world.
+     *
+     * @return the player
+     */
+    Player getPlayer();
+
+    /** Removes all game objects and projectiles from the world. */
+    void clearAll();
+
+    /**
+     * Handles player/door collisions.
+     * 
+     * @param door the door that the player collided with
+     */
+    void onDoorCollision(DoorUnmodifiable door);
+
+}
