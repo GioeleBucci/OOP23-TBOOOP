615197eb0e6005bef3d1069b01220941a641da3d
Gioele Bucci <gioele.bucci@studio.unibo.it>
2024-02-06 10:38:45 +0100

    feat: add a FloorManager helper class
    This class helps the world to handle events regarding room/floor updates

---
+++ src/main/java/tbooop/controller/api/FloorManager.java
@@ -0,0 +1,22 @@
+package tbooop.controller.api;
+
+import tbooop.model.dungeon.rooms.api.DoorUnmodifiable;
+
+/**
+ * The FloorManager interface is responsible for managing the floors and rooms
+ * of the game.
+ */
+public interface FloorManager extends ControllerComponent {
+
+    /**
+     * Handles player/door collisions.
+     * 
+     * @param door the door that the player collided with
+     */
+    void onDoorCollision(DoorUnmodifiable door);
+
+    /**
+     * Sets the floor to a new, more difficult, one.
+     */
+    void changeFloor();
+}
