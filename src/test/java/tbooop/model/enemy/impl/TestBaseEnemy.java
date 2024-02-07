f7c7771865f882126b052291d7f40cd875975557
Simone Mosconi <simone.mosconi2@studio.unibo.it>
2024-02-07 16:52:56 +0100

    test: update tests to prevent errors.

--- src/test/java/tbooop/model/enemy/impl/TestBaseEnemy.java
+++ src/test/java/tbooop/model/enemy/impl/TestBaseEnemy.java
@@ -24,6 +24,7 @@ class TestBaseEnemy {
     private static final Point2d BASE_POS = new Point2dImpl(0, 0);
     private static final int ENEMY_HP = 3;
     private static final int PLAYER_HP = 3;
+    private static final double BASE_RADIUS = 15;
 
     private final Player player = new PlayerImpl(
         BASE_POS, new HealthImpl(PLAYER_HP), 1.0);
@@ -32,7 +33,7 @@ class TestBaseEnemy {
     @BeforeEach
     void initEnemy() {
         this.enemy = new BaseEnemy(
-            BASE_POS, new HealthImpl(ENEMY_HP), 1.0, new ChasingAi(this.player), EnemyType.MELEE);
+            BASE_POS, new HealthImpl(ENEMY_HP), 1.0, new ChasingAi(this.player), EnemyType.MELEE, BASE_RADIUS);
     }
 
     @Test
