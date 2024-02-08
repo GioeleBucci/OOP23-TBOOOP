b9c73bb0d83ee5db933c37e9f37ce601e1da858e
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2024-02-08 17:28:23 +0100

    feat: add to BaseSpriteProviderImpl class the case for a Pickupable gameObject

--- src/main/java/tbooop/view/BaseSpriteProviderImpl.java
+++ src/main/java/tbooop/view/BaseSpriteProviderImpl.java
@@ -3,13 +3,17 @@ package tbooop.view;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import tbooop.model.core.api.GameObjectUnmodifiable;
+import tbooop.model.pickupables.Pickupable;
 import tbooop.model.player.api.PlayerProjectile;
 import tbooop.view.api.BaseSpriteProvider;
+import tbooop.view.pickupables.ItemRender;
 
 /**
  * Basic implementation of BaseSpriteLoader.
  */
 public class BaseSpriteProviderImpl implements BaseSpriteProvider {
+
+    private final ItemRender itemRender = new ItemRender();
 
     /** {@inheritDoc} */
     @Override
@@ -21,6 +25,11 @@ public class BaseSpriteProviderImpl implements BaseSpriteProvider {
                     imgView.setImage(new Image("projectile/playerproj.png"));
                 } else {
                     imgView.setImage(new Image("projectile/enemyproj.png"));
+                }
+            }
+            case PICKUP -> {
+                if (gameObj instanceof Pickupable) {
+                    imgView.setImage(itemRender.getPickupableSprite((Pickupable) gameObj));
                 }
             }
             case TRAPDOOR -> imgView.setImage(new Image("tileset/trapdoor.png"));
