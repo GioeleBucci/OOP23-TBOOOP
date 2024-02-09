fd3b05f35189d09732d47a6578243251515220d1
Gioele Bucci <gioele.bucci@studio.unibo.it>
2024-02-09 22:30:25 +0100

    feat: now player can move and shoot at the same time

--- src/main/java/tbooop/controller/ControllerImpl.java
+++ src/main/java/tbooop/controller/ControllerImpl.java
@@ -28,8 +28,9 @@ import javafx.application.Platform;
 public final class ControllerImpl implements Controller {
 
     private final Logger logger = Logger.getLogger(ControllerImpl.class.getName());
-    private static final int COMMAND_QUEUE_SIZE = 40;
-    private final BlockingQueue<PlayerCommand> cmdQueue = new ArrayBlockingQueue<>(COMMAND_QUEUE_SIZE);
+    private static final int COMMAND_QUEUE_SIZE = 100;
+    private final BlockingQueue<PlayerCommand> moveQueue = new ArrayBlockingQueue<>(COMMAND_QUEUE_SIZE);
+    private final BlockingQueue<PlayerCommand> shootQueue = new ArrayBlockingQueue<>(COMMAND_QUEUE_SIZE);
     private final View view;
     private final World world;
     private boolean playerAdded;
@@ -70,8 +71,11 @@ public final class ControllerImpl implements Controller {
     }
 
     private synchronized void processInput() {
-        if (!cmdQueue.isEmpty()) {
-            cmdQueue.poll().execute(world.getPlayer());
+        if (!moveQueue.isEmpty()) {
+            moveQueue.poll().execute(world.getPlayer());
+        }
+        if (!shootQueue.isEmpty()) {
+            shootQueue.poll().execute(world.getPlayer());
         }
     }
 
@@ -146,7 +150,11 @@ public final class ControllerImpl implements Controller {
     /** {@inheritDoc} */
     @Override
     public void notifyCommand(final PlayerCommand cmd) {
-        this.cmdQueue.add(Objects.requireNonNull(cmd));
+        if (cmd instanceof MoveCommand) {
+            this.moveQueue.add(cmd);
+        } else {
+            this.shootQueue.add(cmd);
+        }
     }
 
     private void waitForNextFrame(final long startTime) {
