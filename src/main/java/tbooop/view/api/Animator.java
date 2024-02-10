449d29868563dd49f0c5ce6ccc1c938e87bd5ba4
Simone Mosconi <simone.mosconi2@studio.unibo.it>
2024-02-10 14:19:00 +0100

    doc: modify Animator javadoc.

--- src/main/java/tbooop/view/api/Animator.java
+++ src/main/java/tbooop/view/api/Animator.java
@@ -1,12 +1,13 @@
 package tbooop.view.api;
 
 /**
- * Interface for animating objects.
+ * An animator stores the logic that allows the view
+ * to display a graphical animation of certain GameObjects.
  */
 public interface Animator {
 
     /**
-     * Updates the state of the animator.
+     * Updates the state of the animation.
      */
     void update();
 }
