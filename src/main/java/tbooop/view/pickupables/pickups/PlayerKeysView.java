73f7733904770fecdef2ab6d0e39475b6f2e2ae2
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2024-02-10 13:35:02 +0100

    feat: add PlayerKeysView representing the number of player keys

---
+++ src/main/java/tbooop/view/pickupables/pickups/PlayerKeysView.java
@@ -0,0 +1,57 @@
+package tbooop.view.pickupables.pickups;
+
+import javafx.scene.layout.HBox;
+import tbooop.model.player.api.UnmodifiablePlayer;
+import tbooop.view.api.ViewComponent;
+import tbooop.view.api.ViewElements;
+
+/**
+ * Represents view of the number of keys
+ * owned by the player.
+ */
+public class PlayerKeysView extends ViewComponent {
+
+    private final HBox root = new HBox();
+    private final PlayerKeysRender keyRender;
+    private final UnmodifiablePlayer player;
+    private int currentKeys;
+    /**
+     * Represents a view for displaying player's keys.
+     *
+     * @param view The ViewElements object used for rendering the view.
+     * @param player The UnmodifiablePlayer object representing the player.
+     */
+    public PlayerKeysView(final ViewElements view, final UnmodifiablePlayer player) {
+        super(view);
+        this.player = player; 
+        view.getRoot().getChildren().add(root);
+        keyRender = new PlayerKeysRender(this.player.getKey(), this.root);
+        this.currentKeys = this.player.getKey();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public synchronized void update() {
+        if (this.currentKeys != this.player.getKey()) {
+            updateLabel(this.player.getKey());
+        }
+        this.currentKeys = this.player.getKey();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'init'");
+    }
+
+    /**
+     * Update key label content according
+     * to the correct number of keys.
+     * 
+     * @param keysNumber number of keys to update.
+     */
+    public synchronized void updateLabel(final int keysNumber) {
+        keyRender.updateLabelText(keysNumber);
+    }
+}
