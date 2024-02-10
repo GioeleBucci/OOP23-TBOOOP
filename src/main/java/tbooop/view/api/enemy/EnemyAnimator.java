107fd1c10bf6b00cbd6d12e74399054c3b0a4c74
Simone Mosconi <simone.mosconi2@studio.unibo.it>
2024-02-10 14:18:35 +0100

    refactor: EnemyAnimator now extends Animator interface.

--- src/main/java/tbooop/view/api/enemy/EnemyAnimator.java
+++ src/main/java/tbooop/view/api/enemy/EnemyAnimator.java
@@ -1,14 +1,10 @@
 package tbooop.view.api.enemy;
 
+import tbooop.view.api.Animator;
+
 /**
  * An enemy animator stores the logic that allows the view
  * to display a graphical animation of enemies.
  */
-public interface EnemyAnimator {
-
-    /**
-     * Updates the animation frames of enemies in the map.
-     */
-    void update();
-
+public interface EnemyAnimator extends Animator {
 }
