48af0f9ad3f7a70fbdd0d782021057bf346dfbfa
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2024-02-11 15:11:55 +0100

    feat: PlayerCoinsRender and PlayerKeysRender now extend  an abstract render class

--- src/main/java/tbooop/view/pickupables/pickups/PlayerCoinsRender.java
+++ src/main/java/tbooop/view/pickupables/pickups/PlayerCoinsRender.java
@@ -3,17 +3,13 @@ package tbooop.view.pickupables.pickups;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
+import tbooop.view.pickupables.PlayerPickupsRenderAbs;
 /**
  * This class is responsible for updating 
  * the view of the number of coins owned by the player.
  */
-public class PlayerCoinsRender {
-    private static final double COIN_SIZE = 1.5;
-    private static final double LABEL_SIZE = 1.0;
+public class PlayerCoinsRender extends PlayerPickupsRenderAbs {
     private static final double COIN_IMAGE_LAYOUT = 60.0;
-    private final Font coinFont = new Font("Serif", 18);
     private final ImageView coinView;
     private final Label coinLabel;
      /**
@@ -29,44 +25,13 @@ public class PlayerCoinsRender {
     }
 
     private void init(final HBox root) {
-        bindKey(coinView, root);
-        bindLabel(coinLabel, root);
+        super.bindPickup(coinView, root, COIN_IMAGE_LAYOUT);
+        super.bindLabel(coinLabel, root);
     }
 
-    /**
-     * Update the label containing the number
-     * of coins.
-     * 
-     * @param amountToUpdate number of coins to update.
-     */
+    /** {@inheritDoc} */
+    @Override
     public void updateLabelText(final int amountToUpdate) {
         this.coinLabel.setText(Integer.toString(amountToUpdate));
-    }
-
-    private void bindKey(final ImageView coinView, final HBox root) {
-        root.getChildren().add(coinView);
-        root.setLayoutY(COIN_IMAGE_LAYOUT);
-
-        coinView.fitWidthProperty()
-        .bind(root.getScene()
-        .widthProperty()
-        .multiply(coinView.getImage().getWidth() / root.getScene().widthProperty().get()));
-
-        coinView.fitHeightProperty()
-        .bind(root.getScene()
-        .heightProperty()
-        .multiply(coinView.getImage().getHeight() / root.getScene().heightProperty().get()));
-
-        coinView.setScaleX(COIN_SIZE);
-        coinView.setScaleY(COIN_SIZE);
-
-    }
-
-    private void bindLabel(final Label label, final HBox root) {
-        label.setFont(coinFont);
-        label.setTextFill(Color.GREY);
-        label.setScaleX(LABEL_SIZE);
-        label.setScaleY(LABEL_SIZE);
-        root.getChildren().add(label);
     }
 }
--- src/main/java/tbooop/view/pickupables/pickups/PlayerKeysRender.java
+++ src/main/java/tbooop/view/pickupables/pickups/PlayerKeysRender.java
@@ -3,21 +3,16 @@ package tbooop.view.pickupables.pickups;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
+import tbooop.view.pickupables.PlayerPickupsRenderAbs;
 
 /**
  * This class is responsible for updating 
  * the view of the number of keys owned by the player.
  */
-public class PlayerKeysRender {
-    private static final double KEY_SIZE = 1.5;
-    private static final double LABEL_SIZE = 1.0;
+public class PlayerKeysRender extends PlayerPickupsRenderAbs {
     private static final double KEY_IMAGE_LAYOUT = 30.0;
-    private final Font keyFont = new Font("Serif", 18);
     private final ImageView keyView;
     private final Label keyLabel;
-
     /**
      * This class is responsible for rendering player's keys number.
      *
@@ -31,44 +26,13 @@ public class PlayerKeysRender {
     }
 
     private void init(final HBox root) {
-        bindKey(keyView, root);
-        bindLabel(keyLabel, root);
+        super.bindLabel(keyLabel, root);
+        super.bindPickup(keyView, root, KEY_IMAGE_LAYOUT);
     }
 
-    /**
-     * Update the label containing the number
-     * of keys.
-     * 
-     * @param amountToUpdate key number to update.
-     */
+    /** {@inheritDoc} */
+    @Override
     public void updateLabelText(final int amountToUpdate) {
         this.keyLabel.setText(Integer.toString(amountToUpdate));
-    }
-
-    private void bindKey(final ImageView keyView, final HBox root) {
-        root.getChildren().add(keyView);
-        root.setLayoutY(KEY_IMAGE_LAYOUT);
-
-        keyView.fitWidthProperty()
-        .bind(root.getScene()
-        .widthProperty()
-        .multiply(keyView.getImage().getWidth() / root.getScene().widthProperty().get()));
-
-        keyView.fitHeightProperty()
-        .bind(root.getScene()
-        .heightProperty()
-        .multiply(keyView.getImage().getHeight() / root.getScene().heightProperty().get()));
-
-        keyView.setScaleX(KEY_SIZE);
-        keyView.setScaleY(KEY_SIZE);
-
-    }
-
-    private void bindLabel(final Label label, final HBox root) {
-        label.setFont(keyFont);
-        label.setTextFill(Color.GREY);
-        label.setScaleX(LABEL_SIZE);
-        label.setScaleY(LABEL_SIZE);
-        root.getChildren().add(label);
     }
 }
