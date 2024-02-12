c1eaa0c967b733454736e7f7dc012d5de6923f3b
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2024-02-13 00:17:01 +0100

    refactor: renamed UnmovableName enum to PickupableName

---
+++ src/main/java/tbooop/model/pickupables/PickupableName.java
@@ -0,0 +1,25 @@
+package tbooop.model.pickupables;
+
+/** Names for every unmovable object
+ *  in the game.
+ */
+public enum PickupableName {
+    /**Bill pickup name. */
+    BILL,
+    /**Coin pickup name. */
+    COIN,
+    /**Heart pickup name. */
+    HEART,
+    /**Key pickup name. */
+    KEY,
+    /**GlassHeart item name. */
+    GLASS_HEART,
+    /**LockedRing item name. */
+    LOCKED_RING,
+    /**SPicySouce item name. */
+    SPICY_SAUCE,
+    /**Zap item name. */
+    ZAP,
+    /**GoldenApple item name. */
+    GOLDEN_APPLE;
+}
