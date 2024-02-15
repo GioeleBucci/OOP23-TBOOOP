d2d1b515dde75219be5b93d485aca9281d2b6510
Gioele Bucci <gioele.bucci@studio.unibo.it>
2024-02-15 13:55:18 +0100

    chore: add some tests in dungeon package

---
+++ src/test/java/tbooop/model/dungeon/doors/TestSpecialDoor.java
@@ -0,0 +1,57 @@
+package tbooop.model.dungeon.doors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import tbooop.commons.api.CardinalDirection;
+import tbooop.model.dungeon.doors.api.DoorPositions;
+import tbooop.model.dungeon.doors.impl.SpecialDoor;
+import tbooop.model.dungeon.rooms.api.Room;
+import tbooop.model.dungeon.rooms.impl.ItemRoom;
+
+class TestSpecialDoor {
+
+    private Room room;
+    private SpecialDoor doorA;
+    private SpecialDoor doorB;
+
+    @BeforeEach
+    void initRoom() {
+        room = new ItemRoom();
+        doorA = new SpecialDoor(DoorPositions.TOP.getPosition(), room);
+        // doorA takes to room, and doorB takes back.
+        doorB = new SpecialDoor(DoorPositions.BOTTOM.getPosition(), new ItemRoom());
+        room.addDoor(CardinalDirection.DOWN, doorB);
+    }
+
+    @Test
+    void checkFields() {
+        assertTrue(doorA.isSpecial());
+        assertEquals(room, doorA.getRoom()); // doorA takes to room
+        assertTrue(doorA.isLocked());
+        assertFalse(doorA.isOpen());
+    }
+
+    @Test
+    void testDoorLock() {
+        doorA.open();
+        assertTrue(doorA.isLocked()); // still locked
+        assertFalse(doorA.isOpen()); // since the door is still locked, it should not open
+        doorA.unlock();
+        assertFalse(doorA.isLocked() || doorB.isLocked()); // the linked door should be unlocked as well
+        assertTrue(doorA.isOpen() && doorB.isOpen()); // unlocked doors should also open
+    }
+
+    @Test
+    void testUnlockOnce() {
+        testDoorLock();
+        doorA.close();
+        assertFalse(doorA.isLocked()); // the door closes but remains unlocked
+        doorA.open();
+        assertTrue(doorA.isOpen() && !doorA.isLocked());
+    }
+}
