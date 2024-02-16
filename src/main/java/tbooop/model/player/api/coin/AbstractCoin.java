c5bb914e921ec3e23837ab32f67ba47fe93423f0
Edoardo Ceresi <edoardo.ceresi@studio.unibo.it>
2024-02-16 10:13:03 +0100

    refactor: abstract concept of coin

---
+++ src/main/java/tbooop/model/player/api/coin/AbstractCoin.java
@@ -0,0 +1,25 @@
+package tbooop.model.player.api.coin;
+
+public abstract class AbstractCoin implements Coin {
+    
+    private int coins;
+
+    public AbstractCoin(int coins) {
+        this.coins = coins;
+    }
+
+    @Override
+    public int getCoins() {
+        return coins;
+    }
+
+    @Override
+    public void addCoins(int coins) {
+        this.coins += coins;
+    }
+
+    @Override
+    public void consumeCoins(int coins) {
+        this.coins -= coins;
+    }
+}
