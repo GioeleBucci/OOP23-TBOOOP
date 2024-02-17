d9571869f74281792a84cbdbe78ad95b46ecdc41
Edoardo Ceresi <edoardo.ceresi@studio.unibo.it>
2024-02-17 11:40:09 +0100

    refactor: change some dependencies

--- src/main/java/tbooop/model/player/api/AbstractPlayer.java
+++ src/main/java/tbooop/model/player/api/AbstractPlayer.java
@@ -55,19 +55,16 @@ public abstract class AbstractPlayer extends AbstractEntity implements Player {
     }
 
     /** {@inheritDoc} */
-    @Override
     public int getCoins() {
         return this.coins.getCoins();
     }
 
     /** {@inheritDoc} */
-    @Override
     public void addCoins(final int coins) {
         this.coins.addCoins(coins);
     }
 
     /** {@inheritDoc} */
-    @Override
     public void consumeCoins(final int coins) {
         this.coins.consumeCoins(coins);
     }
@@ -79,7 +76,6 @@ public abstract class AbstractPlayer extends AbstractEntity implements Player {
     }
 
     /** {@inheritDoc} */
-    @Override
     public boolean hasKey() {
         return this.keys.hasKey();
     }
@@ -101,13 +97,11 @@ public abstract class AbstractPlayer extends AbstractEntity implements Player {
     }
 
     /** {@inheritDoc} */
-    @Override
     public void pickupKey() {
         this.keys.pickupKey();
     }
 
     /** {@inheritDoc} */
-    @Override
     public void useKey() {
         if (hasKey()) {
             this.keys.useKey();
